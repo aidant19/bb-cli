bf8aae188ea3ae0b38481df59d288a0a3c77f7e8
aidant19 <aidant19@gmail.com>
2023-12-28 20:30:46 -0500

    Third script iteration - Corrected AJ resource path injection - File loading and exporting waits on last - Error check for AJ not loaded - Close BB on script termination
    Co-authored-by: Afro <TheAfroOfDoom@users.noreply.github.com>

--- bb-cli.js
+++ bb-cli.js
@@ -1,5 +1,5 @@
 const consoleLogJson = (args) => {
-  console.log(JSON.stringify(args));
+    console.log(JSON.stringify(args));
 }
 
 BBPlugin.register('bb-cli', {
@@ -14,25 +14,34 @@ BBPlugin.register('bb-cli', {
         // TODO: this shouldn't sleep, it should instead listen for AJ's
         // mod injection event or something
         setTimeout(() => {
-          var [blockbenchPath, ...ARGV] = electron.getGlobal("process").argv;
-          var scriptIndicator = ARGV.indexOf("--ajexport");
-          if (scriptIndicator !== -1) {
-              
-              var dir = ARGV[scriptIndicator + 1].concat('/');
-              var target = getConfigPackPaths('C:\\Users\\Aidan\\Desktop\\flowey_remaster\\bb-cli\\config.json');
-              console.log("Target paths: ", target);
-              var files = fs.readdirSync(dir);
-              files.forEach(file => {
-                consoleLogJson({file});
-                  if (file.includes('ajmodel')) {
-                      var content = fs.readFileSync(dir.concat(file), 'utf-8');
-                      var name = file.split('/').pop();
-                      var fileObj = { path:file, content:injectModelPackPaths(content, target), name:name };
-                      loadModelFile(fileObj);
-                      AnimatedJava.API.safeExportProject();
-                  }
-              });
-              //process.exit();
+            var [blockbenchPath, ...ARGV] = electron.getGlobal("process").argv;
+            var scriptIndicator = ARGV.indexOf("--ajexport");
+            if (scriptIndicator !== -1) {
+                if (typeof AnimatedJava === 'undefined') {
+                    throw new Error("Failed to load Animated Java plugin before CLI plugin")
+                }
+
+                var dir = ARGV[scriptIndicator + 1].concat('/');
+                var target = getConfigPackPaths('C:\\Users\\Aidan\\Desktop\\flowey_remaster\\bb-cli\\config.json');
+                console.log("Target paths: ", target);
+                var files = fs.readdirSync(dir).filter((file) => file.includes('ajmodel'));
+                const exportNextFile = () => {
+                    if (Project) { Project.close(); }
+                    const file = files.pop();
+                    if(typeof file === 'undefined') {
+                        window.close();
+                        return;
+                    }
+                    consoleLogJson({ file });
+                    if (file.includes('ajmodel')) {
+                        var content = fs.readFileSync(dir.concat(file), 'utf-8');
+                        var name = file.split('/').pop();
+                        var fileObj = { path: file, content: injectModelPackPaths(content, target), name: name };
+                        loadModelFile(fileObj);
+                        AnimatedJava.API.safeExportProject(exportNextFile);
+                    }
+                };
+                exportNextFile();
             }
         }, 1000);
     }
@@ -56,15 +65,28 @@ function injectModelPackPaths(modelContent, paths) {
     var f = JSON.parse(modelContent);
     f.animated_java.settings.resource_pack_mcmeta = paths[0];
     f.animated_java.exporter_settings["animated_java:datapack_exporter"].datapack_mcmeta = paths[1];
-    for(const texture of f.textures) {
-        if(texture.path.includes(".minecraft")){
+    for (const texture of f.textures) {
+        texture.path = texture.path.replaceAll('\\', '/');
+        // TODO this should maybe be regex?
+        if (texture.path.includes(".minecraft")) {
             const x = texture.path.split("assets")[1];
-            const newPath = `${paths[2]}assets/${x}`;
-            console.log({
-                texturePath: texture.path,
-                afterAssets: x,
-                newPath,
-            });
+            const newPath = `${paths[2]}/assets${x}`;
+            // consoleLogJson({
+            //     texturePath: texture.path,
+            //     afterAssets: x,
+            //     newPath,
+            // });
+            texture.path = newPath;
+        } else if (texture.path.includes("resourcepack/assets")) {
+            const x = texture.path.split("resourcepack/assets")[1];
+            const resourcePackBase = paths[0].split("resourcepack")[0];
+            const newPath = `${resourcePackBase}resourcepack/assets${x}`;
+            // consoleLogJson({
+            //     texturePath: texture.path,
+            //     resourcePackBase,
+            //     afterAssets: x,
+            //     newPath,
+            // });
             texture.path = newPath;
         }
     }
